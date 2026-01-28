4f5d67673552e87a8ab0fc8fbf29bd1348c80b80
khoinh1 <khoinh1@forlife.vn>
2026-01-28 15:46:21 +0700

    trigger build v3

--- app.go
+++ app.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	AppVersionInt           = 2
+	AppVersionInt           = 3
 	BASE_URL                = "https://almost-tend-excitement-kijiji.trycloudflare.com/api"
 	WINDOW_CHECK_UPDATE_URL = BASE_URL + "/app-settings/window"
 )
