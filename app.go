6abe623c226b3b436db898cf0589619bd1c6849f
khoinh1 <khoinh1@forlife.vn>
2026-01-28 09:58:49 +0700

    trigger build v1

--- app.go
+++ app.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-    AppVersionInt           = 2
+    AppVersionInt           = 1
     BASE_URL                = "https://insurance-energy-whale-describes.trycloudflare.com/api"
     WINDOW_CHECK_UPDATE_URL = BASE_URL + "/app-settings/window"
 )
