2629762caaa6785aeea63f008e2c22b9e6796536
khoinh1 <khoinh1@forlife.vn>
2026-01-28 13:38:33 +0700

    trigger build v2

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
 //export const BASE_URL = "https://calm-beds-carry.loca.lt/api";
 export const BASE_URL =
-  "https://insurance-energy-whale-describes.trycloudflare.com/api";
+  "https://idle-income-encyclopedia-procedures.trycloudflare.com/api";
